5b96a4553233432b42f9a6f2c4399f1406606f59
utp <nuevo@correo.com>
2019-02-08 18:47:04 -0500

    data SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Category (id, name, restriction, priority) VALUES (1, 'A',999, 1);
+INSERT INTO Category (id, name, restriction, priority) VALUES (2, 'B',4, 2);
+INSERT INTO Category (id, name, restriction, priority) VALUES (3, 'C',2, 3);
+INSERT INTO Adviser (id, name) VALUES (4, 'Juan');
